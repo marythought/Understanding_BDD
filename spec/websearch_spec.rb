07fd0b1649eb2944c36bf4bc49357849c58f282e
Mary Dickson Diaz <mdickson@gmail.com>
2015-08-31 17:10:05 -0700

    working index page

--- spec/websearch_spec.rb
+++ spec/websearch_spec.rb
@@ -1,9 +1,27 @@
 require "spec_helper"
 
 describe "google page" do
-  it "has results" do
+  it "has content" do
     visit('http://www.google.com')
     page.has_content?('google')
+  end
+end
+
+describe "my webpage" do
+  it "has content" do
+    visit('http://www.marydickson.com')
+    page.has_content?('mary dickson diaz')
+  end
+
+  it "does not have missing content" do
+    visit('http://www.marydickson.com')
+    expect(page).to_not have_content('go away i hate you')
+  end
+
+  it "can click a link" do
+    visit('http://www.marydickson.com')
+    click_link("About")
+    page.has_content?('I’m learning how to code')
   end
 end
 
